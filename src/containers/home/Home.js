1fda4e00be005fd5c3158974aef0c389c2511748
Chelaka <chelakalakshitha@gmail.com>
2024-03-07 02:18:07 +0530

    HomePageSetup

---
+++ src/containers/home/Home.js
@@ -0,0 +1,12 @@
+import { Container } from "react-bootstrap";
+
+const Home = () => {
+  return (
+    <Container>
+      <h1>Home</h1>
+      <p>Welcome to the home page.</p>
+    </Container>
+  );
+}
+
+export default Home;
